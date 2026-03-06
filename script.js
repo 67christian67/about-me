7228cc2a1f8284da9f71c3767eddd29743b4b1f1
67christian67 <wilsonc29@tewksbury.k12.ma.us>
2026-03-06 08:57:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,44 @@
 new Typed("#typing",{
+
 strings:[
 "Web Developer",
+"Creative Coder",
 "UI Designer",
-"JavaScript Builder",
 "Future Software Engineer"
 ],
-typeSpeed:50,
-backSpeed:30,
+
+typeSpeed:60,
+backSpeed:40,
 loop:true
+
 })
 
 particlesJS("particles-js",{
 
 particles:{
-
-number:{value:90},
-
+number:{value:100},
 size:{value:3},
-
 move:{speed:2},
-
-line_linked:{
-enable:true,
-opacity:0.2
+line_linked:{enable:true,opacity:0.2}
 }
+
+})
+
+const bars = document.querySelectorAll(".bar div")
+
+const observer = new IntersectionObserver(entries=>{
+
+entries.forEach(entry=>{
+
+if(entry.isIntersecting){
+
+const width = entry.target.getAttribute("data-width")
+entry.target.style.width = width
 
 }
 
 })
+
+})
+
+bars.forEach(bar=>observer.observe(bar))
